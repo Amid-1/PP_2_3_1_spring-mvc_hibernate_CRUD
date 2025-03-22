36be4f90ce8c1036af6880e8faa9991a56a3699f
Amid-1 <amid.1spbtj@gmail.com.com>
2025-03-22 16:49:47 +0300

    Обновление контроллера и html

--- src/main/java/web/dto/UserDto.java
+++ src/main/java/web/dto/UserDto.java
@@ -24,7 +24,6 @@ public class UserDto {
         this.password = password;
     }
 
-    // Геттеры и сеттеры
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
