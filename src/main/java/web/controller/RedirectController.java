984a0ddbb48305080eac193e6a458c2d961f8fbb
Amid-1 <amid.1spbtj@gmail.com.com>
2025-03-23 20:28:35 +0300

    added RedirectController

--- src/main/java/web/controller/RedirectController.java
+++ src/main/java/web/controller/RedirectController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RedirectController {
-
-    // При заходе на localhost:8080 — сразу редиректим на /users
+    
     @GetMapping("/")
     public String redirectToUsers() {
         return "redirect:/users";
